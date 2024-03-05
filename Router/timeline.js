ca2d6cacb371bb5974d19bcf0378d3163da89d57
Akhras <65845301+Akhras4@users.noreply.github.com>
2024-03-05 11:46:04 +0100

    update "controller"

--- Router/timeline.js
+++ Router/timeline.js
@@ -1,26 +1,13 @@
 const { Router } = require('express'); 
 const app = Router(); 
-const Blog=require('../module/blog')
+const timeFun=require('../controller/controllerPages')
 
 
-app.post("/timeline",(req,res,next)=>{
-    let ti = new Date();
-    const name=req.body.UserName
-    console.log(name)
-    const ms=req.body.ms
-    console.log(ms)
-    const blog = new Blog ({
-      name:name,
-      createdAt: ti,
-      message:ms,
-    });
-    blog.save()
-    console.log(blog)
-    Blog.find()
-        .then((result)=>res.render("timeline",{ result}))
-        .catch((err)=>{console.log(err)})
-  })
-
+app.route("/")
+    .get(timeFun.home)
+    
+app.route("/timeline")
+   .post(timeFun.getData)
 
 
   module.exports = app;
